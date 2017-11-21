1e584c64febb393ca3815862228b6dceec6e81d8
sgyyamn <1042071454@qq.com>
2017-11-21 22:58:06 +0800

    重构

--- controller.py
+++ controller.py
@@ -12,15 +12,12 @@ class controller:
         self.render = web.template.render(self.templates_root)
     def GET(self, menuId):
 
-        #当用户发送选择信息的数字后，点击链接跳转到这里
+        #当用户发送选择信息的数字后，点击链接跳转到这里，menuID代表用户选择的菜单
         print menuId
         id = int(menuId)
-        if id==1:
-            #返回查询成绩的登录页面
-            return self.render.bindPage(id)
-        elif id==2:
-            #返回查询借阅信息的登录页面
-            return self.render.searchBookInfo(id)
+
+        return self.render.bindPage(id)
+
     def POST(self, id):
         
         data = web.input()
@@ -31,13 +28,13 @@ class controller:
         #查询课程信息
         if id==1:
             content, state = sp.getCourseInfo('http://zhjw.dlnu.edu.cn/xkAction.do', stu_num, stu_pwd)
-
-            if (state == 0):
-                return u"大兄弟，信息错误， 俺无能为力啊".encode("gb2312")
-            else:
-                return content.encode("gb2312")
         #查询图书借阅信息
+        elif id==2:
+            content, state = sp.getBookInfo()
 
-        if id==12:
+        if (state == 0):
+            return u"大兄弟，信息错误， 俺无能为力啊".encode("gb2312")
+        else:
+            return content.encode("gb2312")
 
 
